d2dd632d2a16c770cb408b094575a6787f4a5096
sh810000 <ductoandz133@gmail.com>
2026-09-11 17:02:30 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -125,9 +125,32 @@ TITLE=$KERNEL_NAME-$KERNEL_VERSION
 
 cd $KERNEL_DIR
 
-msg "KernelSU"
+msg "KernelSU & SUSFS"
 if [[ $KSU_ENABLED == "true" ]]; then
     curl -LSs "https://raw.githubusercontent.com/$KERNELSU_REPO/v3.2.0-legacy/kernel/setup.sh" | bash -s $KSU_TARGET
+
+    # --- BẮT ĐẦU PATCH SUSFS ---
+    msg "Injecting SUSFS Patches"
+    git clone --depth=1 -b kernel-4.14 https://gitlab.com/simonpunk/susfs4ksu.git $WORKDIR/susfs4ksu
+
+    # Copy lõi của susfs vào kernel
+    cp $WORKDIR/susfs4ksu/kernel_patches/fs/susfs.c fs/
+    cp $WORKDIR/susfs4ksu/kernel_patches/include/linux/susfs.h include/linux/
+    
+    # Dùng -F 3 (Fuzz) để bypass lỗi hunk do lệch context trên kernel 4.14
+    patch -p1 -F 3 < $WORKDIR/susfs4ksu/kernel_patches/50_add_susfs_in_kernel-4.14.patch
+    
+    # Patch KernelSU-Next để tương thích SUSFS
+    cd KernelSU-Next
+    patch -p1 -F 3 < $WORKDIR/susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch
+    cd ..
+
+    # Tự động chèn cờ cấu hình SUSFS vào defconfig
+    echo "CONFIG_KSU_SUSFS=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> $DEVICE_DEFCONFIG_FILE
+    echo "CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT=y" >> $DEVICE_DEFCONFIG_FILE
+    # --- KẾT THÚC PATCH SUSFS ---
 
     echo "CONFIG_KPROBES=y" >> $DEVICE_DEFCONFIG_FILE
     echo "CONFIG_HAVE_KPROBES=y" >> $DEVICE_DEFCONFIG_FILE
