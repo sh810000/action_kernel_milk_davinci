34e91574d7c3936523918cccc526e61059f99d96
sh810000 <ductoandz133@gmail.com>
2026-09-11 12:54:33 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -75,7 +75,7 @@ echo "COMMON_DEFCONFIG: $COMMON_DEFCONFIG"
 # Set variables
 WORKDIR="$(pwd)"
 
-CLANG_DLINK="https://github.com/ZyCromerZ/Clang/releases/download/13.0.1-20211126-release/Clang-13.0.1-20211126.tar.gz"
+CLANG_DLINK="https://github.com/ZyCromerZ/Clang/releases/download/14.0.6-20230213-release/Clang-14.0.6-20230213.tar.gz"
 CLANG_DIR="$WORKDIR/Clang/bin"
 
 KERNEL_REPO="${KERNEL_GIT::-4}/"
